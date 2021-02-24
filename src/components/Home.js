3ea6382db09ec4950b8898ca51ac2390586083f7
Sandeep Kumar <kumarsandy923@gmail.com>
2021-02-25 01:46:47 +0530

    fetch data and display error & loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,20 +2,39 @@ import React, { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
   const deleteBlog = (id) => {
     const newBlog = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlog);
   };
   useEffect(() => {
-    fetch(" http://localhost:4040/blogs")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch(" http://localhost:4040/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Couldn't fetch the blogs");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+
+          console.log(err.message);
+        });
+    }, 1000);
   }, []);
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} deleteBlog={deleteBlog} />}
     </div>
   );
